c72c28c17da3a4c81ff12003918100111ab1bc3b
ifisher18 <ifisher18@LAB-SCI-214-14>
2016-11-21 18:45:25 -0800

    changes to weapons class

--- ConsoleApplication1/ConsoleApplication1/Weapon.h
+++ ConsoleApplication1/ConsoleApplication1/Weapon.h
@@ -2,16 +2,33 @@
 #ifndef Weapon_h
 #define Weapon.h
 
-
+#include <iostream>
+using namespace std;
 
 class weapon {
 public:
 	weapon(int attack, int pierce, int armor);
 	void attack(NPC* c);
+	void defend();
+
+	// attack functions
+	int getattack();
+	void setattack();
+	
+	// defend functions
+
+
+	// armor functions
+	int getarmor();
+	void setarmor();
 
 private:
-	int armor, attack, pierce;
-	//endurance?  things break?
+	int armor;
+	int attack;
+	int pierce;
+	int endurance;
+
+
 
 };
 
